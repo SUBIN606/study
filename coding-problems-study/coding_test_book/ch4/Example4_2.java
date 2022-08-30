d988131058fa12c720d4865adcdb421ef81a08a8
subin606 <gaemilee606@gmail.com>
2022-08-30 17:26:42 +0900

    예제 4-2 풀이 수정

--- coding-problems-study/coding_test_book/ch4/Example4_2.java
+++ coding-problems-study/coding_test_book/ch4/Example4_2.java
@@ -13,57 +13,22 @@ public class Example4_2 {
     * 정수 N이 입력되면 00시 00분 00초 부터 N시 59분 59초까지의 모든 시각 중에서
     * 3이 하나라도 포함되는 모든 경우의 수를 구하는 프로그램을 작성하라.
      */
-
     public static int solution(int n) {
-        int hour = count3(n) * 3600;
-        System.out.println("hour = " + hour);
-
-        int min = count3(60) * (n + 1 - count3(n));
-        System.out.println("min = " + min);
-
-        int sec = count3(60) * 60 * (n + 1 - count3(n)) - min;
-        System.out.println("sec = " + sec);
-
-        return hour + min + sec;
-    }
-
-    private static int count3(int end) {
         int count = 0;
-        for (int i = 0; i <= end; i++) {
+        for (int i = 0; i <= n; i++) {
             if (String.valueOf(i).contains("3")) {
-                count++;
+                count += 3600;  // 시간에 3이 포함되면 매 초마다 count가 증가하므로 3600을 더한다.
+            } else {    // 포함되지 않으면 분에 포함되는지 확인한다.
+                for (int j = 0; j < 60; j++) {
+                    if (String.valueOf(j).contains("3")) {  // 분에 3이 포함되면 매 초마다 count가 증가하므로 60을 더한다.
+                        count += 60;
+                    } else {    // 포함되지 않으면 60 중 3이 포함되는 횟수인 15만 더한다.
+                        count += 15;
+                    }
+                }
             }
         }
         return count;
     }
 
-    private static int countHour(int end) {
-        int count = 0;
-        for (int i = 0; i <= end; i++) {
-            if (String.valueOf(i).contains("3")) {
-                count += 60 * 60;
-            }
-        }
-        return count;
-    }
-
-    private static int countMinute(int end) {
-        int count = 0;
-        for (int i = 0; i < end; i++) {
-            if (String.valueOf(i).contains("3")) {
-                count += 60;
-            }
-        }
-        return count;
-    }
-
-    private static int countSecond(int end) {
-        int count = 0;
-        for (int i = 0; i < end; i++) {
-            if (String.valueOf(i).contains("3")) {
-                count++;
-            }
-        }
-        return count;
-    }
 }
