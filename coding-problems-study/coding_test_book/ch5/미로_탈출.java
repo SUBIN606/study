2db0c44daa640db6d3e00e350245779709b64a0e
subin606 <gaemilee606@gmail.com>
2022-08-31 23:10:20 +0900

    5장 실전문제 풀이

---
+++ coding-problems-study/coding_test_book/ch5/미로_탈출.java
@@ -0,0 +1,96 @@
+package coding_test_book.ch5;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class 미로_탈출 {
+
+    // 상 - 우 - 하 - 좌
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+//        int[][] map = {
+//                {1, 0, 1, 0, 1, 0},
+//                {1, 1, 1, 1, 1, 1},
+//                {0, 0, 0, 0, 0, 1},
+//                {1, 1, 1, 1, 1, 1},
+//                {1, 1, 1, 1, 1, 1}
+//        };
+//        int solution = solution(map);
+//        System.out.println("solution = " + solution);
+
+        int[][] map2 = {
+                {1, 0, 0, 1, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 1, 1}
+        };
+        int solution1 = solution(map2);
+        System.out.println("solution1 = " + solution1);
+    }
+
+    /*
+    * N * M 크기의 직사각형 형태의 미로가 있다.
+    * 최초 시작점은 (1, 1)이고, 탈출구는 (N, M)이다.
+    *
+    * 괴물이 있는 부분은 0이고, 괴물이 없는 부분은 1로 표시한다.
+    * 미로는 반드시 탈출할 수 있는 형태로 제시된다.
+    * 탈출하기 위해 움직여야 하는 최소 칸의 개수를 구하라.(시작 칸과 마지막 칸을 모두 포함해 계산한다.)
+    * */
+    public static int solution(int[][] map) {
+        return bfs(map);
+    }
+
+    public static boolean canMove(int[][] map, boolean[][] visited, int x, int y) {
+        int allow = 0;
+        for (int i = 0; i < dx.length; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    allow++;
+                }
+            }
+        }
+        return allow > 0;
+    }
+
+    public static int bfs(int[][] map) {
+        int count = 1;
+        int exitX = map.length - 1, exitY = map[0].length - 1;
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[map.length][map[0].length];
+        queue.add(new int[] {0, 0});
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+
+            if (map[x][y] == 1 && !visited[x][y]) {
+                if (exitX == x && exitY == y) {
+                    break;
+                }
+                if (!canMove(map, visited, x, y)) {
+                    continue;
+                } else {
+                    count++;
+                }
+                visited[x][y] = true;
+                for (int i = 0; i < dx.length; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length && map[nx][ny] == 1) {
+                        queue.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ coding-problems-study/coding_test_book/ch5/음료수_얼려_먹기.java
@@ -0,0 +1,57 @@
+package coding_test_book.ch5;
+
+public class 음료수_얼려_먹기 {
+
+    public static void main(String[] args) {
+        int[][] tray = {
+                {0, 0, 1, 1, 0},
+                {0, 0, 0, 1, 1},
+                {1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0}
+        };
+        solution(tray);
+    }
+
+    /*
+     * N * M 크기의 얼음틀이 있다.
+     * 구멍이 뚫린 부분은 0, 칸막이가 존재하는 부분은 1로 표시된다.
+     * 구멍끼리 상, 하, 좌, 우로 붙어이쓴 경우 서로 연결되어 있는 것으로 간주한다.
+     *
+     * 얼음 틀의 모양이 주어졌을 때 생성되는 총 아이스크림의 개수를 구하는 프로그램을 작성하시오.
+     *
+     * input - 얼음틀
+     * output - 한 번에 만들 수 있는 아이스크림의 개수
+     * */
+    public static int solution(int[][] tray) {
+        int answer = 0;
+
+        int cols = tray[0].length, rows = tray.length;
+        boolean[][] visited = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(tray, cols, rows, i, j, visited)) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static boolean dfs(int[][] tray, int cols, int rows, int x, int y, boolean[][] visited) {
+        if (x <= -1 || x >= rows || y <= -1 || y >= cols) {
+            return false;
+        }
+        if (!visited[x][y] && tray[x][y] == 0) {
+            System.out.println("x = " + x + " y = " + y);
+            visited[x][y] = true;
+            dfs(tray, cols, rows, x - 1, y, visited);
+            dfs(tray, cols, rows, x, y - 1, visited);
+            dfs(tray, cols, rows, x + 1, y, visited);
+            dfs(tray, cols, rows, x, y + 1, visited);
+            return true;
+        }
+        return false;
+    }
+
+}
