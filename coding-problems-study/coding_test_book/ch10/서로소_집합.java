362e9e921ef696f14b0512a039203cb16fc53c95
subin606 <gaemilee606@gmail.com>
2022-09-13 15:32:51 +0900

    서로소 집합 설명 추가

--- coding-problems-study/coding_test_book/ch10/서로소_집합.java
+++ coding-problems-study/coding_test_book/ch10/서로소_집합.java
@@ -25,6 +25,10 @@ public class 서로소_집합 {
         disjointSets(parent, unions);
     }
 
+    /*
+    * 서로소 집합 자료구조란 서로소 부분 집합들로 나누어진 원소들의 데이터를 처리하기 위한 자료구조라 할 수 있다.
+    * 서로소 집합 자료 구조는 union과 find 이 2개의 연산으로 조작할 수 있다.
+    * */
     public static void disjointSets(int[] parent, int[][] unions) {
         for (int[] union : unions) {
             unionParent(parent, union[0], union[1]);
@@ -34,13 +38,15 @@ public class 서로소_집합 {
             System.out.print(findParent(parent, i) + " ");
         }
 
-        System.out.println("");
-        System.out.print("부모 테이블: ");
+        System.out.print("\n부모 테이블: ");
         for (int i = 1; i < parent.length; i++) {
             System.out.print(parent[i] + " ");
         }
     }
 
+    /*
+    * find 연산은 특정한 원소가 속한 집합이 어떤 집합인지 알려주는 연산이다.
+    * */
     private static int findParent(int[] parent, int x) {
         if (x == parent[x]) {
             return x;
@@ -49,9 +55,15 @@ public class 서로소_집합 {
         return parent[x] = findParent(parent, parent[x]);
     }
 
+    /*
+    * union 연산은 2개의 원소가 포함된 집합을 하나의 집합으로 합치는 연산이다.
+    * */
     private static void unionParent(int[] parent, int a, int b) {
         a = findParent(parent, a);
         b = findParent(parent, b);
+
+        // 일반적으로 서로소 집합을 표현할 때는 번호가 큰 노드가 번호가 작은 노드를 간선으로 가리키도록 한다.
+        // 즉 번호가 작은 노드가 부모가 되고, 번호가 큰 노드가 자식이 된다.
         if (a < b) {
             parent[b] = a;
         } else {
